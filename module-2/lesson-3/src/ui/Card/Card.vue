d13b105bf34b05cba54ab6202417755f1c5052bc
mukhriddin-dev <khodievm@gmail.com>
2023-04-27 16:27:02 +0500

    add

---
+++ module-2/lesson-3/src/ui/Card/Card.vue
@@ -0,0 +1,19 @@
+<script>
+export default {
+    name:"Card",
+    props:{
+        data:Object
+    }
+}
+</script>
+
+
+<template>
+    <div class="card w-[260px] h-[340px] bg-white shadow m-5">
+        <img :src="data.avatar" class="w-full" alt="rasm">
+        <div class="card-body">
+        <h1 class="text-center">{{ data.first_name }} {{ data.last_name  }}</h1>
+        <p>{{ data.email }}</p>
+        </div>
+    </div>
+</template>
